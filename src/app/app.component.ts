c23ca502ba610021430bb1b47caad8bc9ceefe8e
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-07-23 14:50:13 -0300

    feat: simple project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teamapp';
+  numberMin: number = 0;
+  numberMax: number = 0;
+  trueValue: number = 0;
+  onInputMin(min: string){
+    this.numberMin = Number(min);
+  }
+  onInputMax(max: string){
+    this.numberMax = Number(max);
+  }
+  onSubmitBtn(){
+    this.trueValue = Math.floor(Math.random() * (this.numberMax - this.numberMin) + this.numberMin);
+  }
 }
